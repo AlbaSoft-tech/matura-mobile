3942e0a1361930642eb09ce897385186ff802f03
Albasoft-tech <albasoft.tech@gmail.com>
2025-08-17 01:15:44 +0200

    first final touch

--- android/app/src/main/java/com/anonymous/matura/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/matura/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.frontEnd
+package com.anonymous.matura
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/anonymous/matura/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/matura/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.frontEnd
+package com.anonymous.matura
 
 import android.app.Application
 import android.content.res.Configuration
